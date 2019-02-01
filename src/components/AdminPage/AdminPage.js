8ab5afcaa9a291f4beed20b276e681e2dafa5349
Anthony D <dunahee@gmail.com>
2019-02-01 16:33:13 -0600

    updated packages, set up router

---
+++ src/components/AdminPage/AdminPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AdminPage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Admin View</h2>
+      </div>
+    )
+  }
+}
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import {HashRouter as Router, Link, Route} from 'react-router-dom';
+import Header from './../Header/Header';
+import ProjectPage from './../ProjectPage/ProjectPage';
+import AdminPage from './../AdminPage/AdminPage';
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-        <p>Empty Page</p>
+        <Header />
+        <Router>
+          <div>
+            <Link to='/'>Projects</Link>
+            <Link to='/admin'>Admin</Link>
+            <Route exact path='/' component={ProjectPage}/>
+            <Route exact path='/admin' component={AdminPage} />
+          </div>
+        </Router>
       </div>
     );
   }
