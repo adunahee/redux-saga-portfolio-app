5e8562847593d80dfae88f061e510893c5ac82c1
Anthony D <dunahee@gmail.com>
2019-02-01 18:09:27 -0600

    show project items with details about project obj

--- src/components/ProjectPage/ProjectItem.js
+++ src/components/ProjectPage/ProjectItem.js
@@ -5,7 +5,12 @@ class ProjectItem extends Component {
   render() {
     return (
       <div>
-        {JSON.stringify(this.props.project)}
+          <p>Name: {this.props.project.name}</p>
+          <p>Description: {this.props.project.description}</p>
+          <img src={this.props.project.thumbnail} />
+          <p>Website: {this.props.project.website}</p>
+          <p>GitHub: {this.props.project.github}</p>
+          <p>Date Finished: {this.props.project.date_completed}</p>
       </div>
     )
   }
